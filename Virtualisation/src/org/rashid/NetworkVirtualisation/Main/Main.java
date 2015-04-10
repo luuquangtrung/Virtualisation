30ddab9ba163bfc0c7155b3994cf4b838eb2326a
bnaudts <bnaudts@kemble>
2015-04-10 17:52:20 +0200

    changed main with pricing for substrate nodes and links

--- Virtualisation/src/org/rashid/NetworkVirtualisation/Main/Main.java
+++ Virtualisation/src/org/rashid/NetworkVirtualisation/Main/Main.java
@@ -6,7 +6,9 @@ import jade.wrapper.StaleProxyException;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.rashid.NetworkVirtualisation.Network.Links.SubstrateLink;
 import org.rashid.NetworkVirtualisation.Network.Networks.SubstrateNetwork;
+import org.rashid.NetworkVirtualisation.Network.Nodes.SubstrateNode;
 import org.rashid.NetworkVirtualisation.Utilities.VirtualisationVocabulary;
 
 public class Main implements VirtualisationVocabulary{
@@ -18,6 +20,17 @@ public class Main implements VirtualisationVocabulary{
 	 s.createBriteTopology(25, "substratenetwork");
 	 
 	 ArrayList<SubstrateNetwork> list = s.copySubstrateNetwork(1);
+	 
+	 //add node and link pricing
+	 SubstrateNetwork dynamicallyPricedSubstrateNetwork = list.get(0);
+	 ArrayList<SubstrateNode> nodes = dynamicallyPricedSubstrateNetwork.getNodes();
+	 ArrayList<SubstrateLink> links = dynamicallyPricedSubstrateNetwork.getLinks();
+	 for (int n = 0; n < nodes.size(); n++){
+		 nodes.get(n).setNodeCost(100.0);
+	 }
+	 for (int l = 0; l < links.size(); l++){
+		 links.get(l).setLinkCost(100.0);
+	 }
 	 	 
 	 VNRequests req = new VNRequests();
 	 	 
